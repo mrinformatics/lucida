2823d4ab8e627454c1028d0ded5da8ec25957546
thestark <mail.saadmemon@gmail.com>
2018-06-07 21:12:28 +0100

    Update install_phantomjs.sh

--- lucida/botframework-interface/deps/install_phantomjs.sh
+++ lucida/botframework-interface/deps/install_phantomjs.sh
@@ -6,13 +6,13 @@ uname -a | grep "x86_64" > /dev/null
 if [ $? -eq 0 ]; then
   wget -c "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2" \
   && tar -xf phantomjs-2.1.1-linux-x86_64.tar.bz2 \
-  && killall phantomjs || : \
+  && sudo killall phantomjs || : \
   && cp phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
   if [ $? -ne 0 ]; then exit 1; fi
 else
   wget -c "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2" \
   && tar -xf phantomjs-2.1.1-linux-i686.tar.bz2 \
-  && killall phantomjs || : \
+  && sudo killall phantomjs || : \
   && cp phantomjs-2.1.1-linux-i686/bin/phantomjs /usr/bin/phantomjs
   if [ $? -ne 0 ]; then exit 1; fi
 fi
