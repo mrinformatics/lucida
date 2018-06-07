8e4f3b878757c1422d0dc8791e7fea9efa94e9c6
thestark <mail.saadmemon@gmail.com>
2018-06-07 21:12:00 +0100

    Update install_ngrok.sh

--- lucida/botframework-interface/deps/install_ngrok.sh
+++ lucida/botframework-interface/deps/install_ngrok.sh
@@ -6,13 +6,13 @@ uname -a | grep "x86_64" > /dev/null
 if [ $? -eq 0 ]; then
   wget -c "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip" \
   && unzip -o ngrok-stable-linux-amd64.zip \
-  && killall ngrok || : \
+  && sudo killall ngrok || : \
   && cp ngrok /usr/bin/ngrok
   if [ $? -ne 0 ]; then exit 1; fi
 else
   wget -c "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip" \
   && unzip ngrok-stable-linux-386.zip \
-  && killall ngrok || : \
+  && sudo killall ngrok || : \
   && cp ngrok /usr/bin/ngrok
   if [ $? -ne 0 ]; then exit 1; fi
 fi
