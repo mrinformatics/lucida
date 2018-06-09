eb673c39bde285fa01473acc1bcce3a4afc369ba
thestark <mail.saadmemon@gmail.com>
2018-06-09 16:43:58 +0100

    Update apt_deps.sh

--- tools/apt_deps.sh
+++ tools/apt_deps.sh
@@ -74,4 +74,12 @@ apt-get install -y zlib1g-dev \
                    libffi-dev \
                    libbz2-dev \
                    python-yaml \
+                   python-gi python3-gi \
+                   gstreamer1.0-tools \
+                   gir1.2-gstreamer-1.0 \
+                   gir1.2-gst-plugins-base-1.0 \
+                   gstreamer1.0-plugins-good \
+                   gstreamer1.0-plugins-ugly \
+                   gstreamer1.0-plugins-bad \
+                   gstreamer1.0-libav \
 && pip install virtualenv ws4py
